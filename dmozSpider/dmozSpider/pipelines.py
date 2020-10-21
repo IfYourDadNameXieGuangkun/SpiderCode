6b5e2abe119a16fd1f6c688099b51263ec097dd4
1 <1>
2020-10-21 11:55:19 +0800

    豆瓣爬虫

--- dmozSpider/dmozSpider/pipelines.py
+++ dmozSpider/dmozSpider/pipelines.py
@@ -5,8 +5,30 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-
+import json
+import pymongo
 class DmozspiderPipeline(object):
+    def __init__(self):
+        self.file = open('item.jl','w',encoding='utf-8')
     def process_item(self, item, spider):
         #print(item)
+        line = json.dumps(dict(item),ensure_ascii=False)+'\n'
+        self.file.write(line)
         return item
+
+
+
+class MongoPipeline(object):
+    collection_item = 'scrapy_item'
+
+    def __init__(self,mongo_url,mongo_db):
+        self.mongo_url = mongo_url
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crwal(cls,crawler):
+        return cls(
+            mongo_url=crawler.settings.get("MONGO_URL"),
+            mongo_db=crawler.setting.get("MONGO_DATABASE",'item')
+        )
+
--- dmozSpider/dmozSpider/settings.py
+++ dmozSpider/dmozSpider/settings.py
@@ -66,6 +66,7 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
    'dmozSpider.pipelines.DmozspiderPipeline': 300,
+   'dmozSpider.pipelines.MongoPipeline': 350,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
